6d48782c5d33774f4a6027489dadfe228ab4a442
patrick <patrick.j.mcpherson@gmail.com>
2021-12-03 18:40:32 -0800

    Implement phase 1
    created #new_move_positions and iterated ::valid_moves

--- W1D2/knights_travails/knight_path_finder.rb
+++ W1D2/knights_travails/knight_path_finder.rb
@@ -4,6 +4,7 @@ class KnightPathFinder
   def initialize(start_position)
     @start_position = start_position
     @root_node = PolyTreeNode.new(@start_position)
+    @considered_positions = [@start_position]
     build_move_tree
   end
 
@@ -17,9 +18,14 @@ class KnightPathFinder
     left_down = [position.first + 1, position.last - 2]
     left_up = [position.first - 1, position.last - 2]
 
-    moves = [up_left, up_right, right_up, right_down, down_right, down_left, left_down, left_up]
+    [up_left, up_right, right_up, right_down, down_right, down_left, left_down, left_up]
+  end
 
-    # TODO more logic to check repeated positions
+  def new_move_positions(position)
+    moves = KnightPathFinder.valid_moves(position)
+    moves.select! { |move| !@considered_positions.include?(move) }
+    @considered_positions += moves
+    moves
   end
 
   def build_move_tree
