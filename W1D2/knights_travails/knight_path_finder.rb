e85cd245b2615f699fd28db107f75c207d3a37fa
patrick <patrick.j.mcpherson@gmail.com>
2021-12-08 07:47:00 -0800

    Implement phase 2

--- W1D2/knights_travails/knight_path_finder.rb
+++ W1D2/knights_travails/knight_path_finder.rb
@@ -5,7 +5,7 @@ class KnightPathFinder
     @start_position = start_position
     @root_node = PolyTreeNode.new(@start_position)
     @considered_positions = [@start_position]
-    build_move_tree
+    build_move_tree(@root_node)
   end
 
   def self.valid_moves(position)
@@ -18,17 +18,22 @@ class KnightPathFinder
     left_down = [position.first + 1, position.last - 2]
     left_up = [position.first - 1, position.last - 2]
 
-    [up_left, up_right, right_up, right_down, down_right, down_left, left_down, left_up]
+    moves = [up_left, up_right, right_up, right_down, down_right, down_left, left_down, left_up]
+
+    moves.select { |move| (0...8).to_a.include?(move.first) && (0...8).to_a.include?(move.last) }
   end
 
   def new_move_positions(position)
     moves = KnightPathFinder.valid_moves(position)
     moves.select! { |move| !@considered_positions.include?(move) }
     @considered_positions += moves
-    moves
+    moves.map { |move| PolyTreeNode.new(move) }
   end
 
-  def build_move_tree
-    # TODO build this method
+  def build_move_tree(start_position)
+    new_move_positions(start_position.value).each do |position|
+      start_position.add_child(position)
+      build_move_tree(position)
+    end
   end
 end
