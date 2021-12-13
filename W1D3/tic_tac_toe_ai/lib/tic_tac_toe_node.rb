a10519a7f4f3c6073acd89786484c6c4d678d9c1
patrick <patrick.j.mcpherson@gmail.com>
2021-12-12 17:22:11 -0800

    Children method

--- W1D3/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
+++ W1D3/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
@@ -1,17 +1,31 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
-  def losing_node?(evaluator)
-  end
+  def losing_node?(evaluator); end
 
-  def winning_node?(evaluator)
-  end
+  def winning_node?(evaluator); end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children = []
+    @board.rows.each_with_index do |row, row_idx|
+      row.each_with_index do |pos, pos_idx|
+        next if @board[[row_idx, pos_idx]]
+        new_board = @board.dup
+        new_board[[row_idx, pos_idx]] = next_mover_mark
+        children <<
+          TicTacToeNode.new(new_board, next_mover_mark == :x ? :o : :x, [row_idx, pos_idx])
+      end
+    end
+    children
   end
 end
