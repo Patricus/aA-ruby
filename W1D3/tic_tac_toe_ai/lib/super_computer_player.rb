024a80f07562e1cb7e300df4aad1fbc8b911e414
patrick <patrick.j.mcpherson@gmail.com>
2021-12-13 20:35:04 -0800

    Implement super_computer_player

--- W1D3/tic_tac_toe_ai/lib/super_computer_player.rb
+++ W1D3/tic_tac_toe_ai/lib/super_computer_player.rb
@@ -2,12 +2,16 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    moves = node.children
+    moves.reject! { |move| move.losing_node?(mark) }
+    moves.find { |move| move.winning_node?(mark) }.prev_move_pos
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  puts "Play the brilliant computer!"
-  hp = HumanPlayer.new("Jeff")
+  puts 'Play the brilliant computer!'
+  hp = HumanPlayer.new('Jeff')
   cp = SuperComputerPlayer.new
 
   TicTacToe.new(hp, cp).run
