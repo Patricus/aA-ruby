15d144904673a01be2c0cd557587532198eff6e5
patrick <patrick.j.mcpherson@gmail.com>
2021-12-19 16:33:53 -0800

    Finish employee

--- W1D5/class_inheritance/employee.rb
+++ W1D5/class_inheritance/employee.rb
@@ -12,10 +12,47 @@ class Employee
 end
 
 class Manger < Employee
-  def initialize(employees)
+  attr_reader :employees
+  def initialize(name, title, salary, boss, employees)
+    super(name, title, salary, boss)
     @employees = employees
   end
+
+  def sub_employees(boss)
+    return [] if boss.class != Manger
+
+    boss
+      .employees
+      .map { |employee| [employee.salary] + sub_employees(employee) }
+      .flatten
+  end
+
   def bonus(multiplier)
-    @employees.flatten.length * multiplier
+    sub_employees(self).sum * multiplier
   end
 end
+
+ned =
+  Manger.new(
+    'Ned',
+    'Founder',
+    1_000_000,
+    nil,
+    [
+      darren =
+        Manger.new(
+          'Darren',
+          'TA Manager',
+          78_000,
+          ned,
+          [
+            shawna = Employee.new('Shawna', 'TA', 12_000, darren),
+            david = Employee.new('David', 'TA', 10_000, darren),
+          ],
+        ),
+    ],
+  )
+
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
